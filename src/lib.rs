1d2edb40ac5353ce5a3f48615d5fb38fb8bd18ac
josipprgic <josip.prgic98@gmail.com>
2024-01-09 15:50:49 +0100

    finish initial server with health check test

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, dev::Server};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!{"Hello {}!", name}
-}
-
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/api/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-    .run();
-
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,31 @@
-#[tokio::test]
-async fn health_check_works() {
-    spawn_app();
-    let client = reqwest::Client::new();
+use std::net::TcpListener;
+use ztp_rust::run;
 
-    let response = client
-    .get("http://localhost:8000/api/health_check")
-        .send()
-        .await
-    .expect("Failed to execute request");
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
 
-fn spawn_app() {
-    let server = ztp_rust::run().expect("Failed to start server");
-    let _ = tokio::spawn(server);
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
